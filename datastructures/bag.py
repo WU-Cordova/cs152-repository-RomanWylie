04840e62ed1b2083794411b409ca03089da70869
RomanWylie <143564289+RomanWylie@users.noreply.github.com>
2025-01-22 12:59:12 -0800

    init and add implemented

--- datastructures/bag.py
+++ datastructures/bag.py
@@ -4,10 +4,17 @@ from datastructures.ibag import IBag, T
 
 class Bag(IBag[T]):
     def __init__(self, *items: Optional[Iterable[T]]) -> None:
-        raise NotImplementedError("__init__ method not implemented")
+        self.__bag: dict[T, int]={}
+        if items:
+            for item in items:
+                self.add(item)
 
     def add(self, item: T) -> None:
-        raise NotImplementedError("add method not implemented")
+        if item in self.__bag:
+            self.__bag[item] += 1
+        else:
+            self.__bag[item] = 1
+    
 
     def remove(self, item: T) -> None:
         raise NotImplementedError("remove method not implemented")
@@ -18,7 +25,7 @@ class Bag(IBag[T]):
     def __len__(self) -> int:
         raise NotImplementedError("__len__ method not implemented")
 
-    def distinct_items(self) -> int:
+    def distinct_items(self) -> Iterable[T]:
         raise NotImplementedError("distinct_items method not implemented")
 
     def __contains__(self, item) -> bool:
