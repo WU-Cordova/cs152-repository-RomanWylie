7a308b1715d01ed636070d422f04c9b164f6d38e
RomanWylie <143564289+RomanWylie@users.noreply.github.com>
2025-02-05 12:32:40 -0800

    array start

--- projects/project1/game.py
+++ projects/project1/game.py
@@ -1,5 +1,13 @@
-from random import randint
+from random import choice
 from deck import Deck
 
 class Game:
-    pass
+    def __init__(self):
+        self.__playerhand = ()
+        self.__dealerhand = ()
+        self.__multiplier = (2,4,6,8)
+    
+    def round_start(self):
+        round_deck = Deck(choice(self.__multiplier))
+
+
