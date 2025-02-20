648cf2cedcb8714e66fcab3623f78b8fabc8d5fc
RomanWylie <143564289+RomanWylie@users.noreply.github.com>
2025-02-19 18:50:53 -0800

    array complete

--- datastructures/array.py
+++ datastructures/array.py
@@ -39,10 +39,13 @@ class Array(IArray[T]):
 
     @overload
     def __getitem__(self, index: int) -> T:
+        if not isinstance(index, int): 
+            raise TypeError("Index must be an int or a slice")
         if index not in range(self.__logical_size):
             raise IndexError("Index must be in array")
         item = self.__items[index]
         return item.item() if isinstance(item, np.generic) else item
+  
         
         
     @overload
@@ -55,21 +58,25 @@ class Array(IArray[T]):
                 raise IndexError("Index must be in array")
             sliced_items = self.__items[start:stop:step]
             return Array(starting_sequence=list(sliced_items), data_type=self.__data_type)
+        else: 
+            raise TypeError("Index must be an int or a slice")
     
     def __getitem__(self, index: int | slice) -> T | Sequence[T]:
         if isinstance(index, slice):
-            start = index.start if not None else 0
-            stop = index.stop if not None else -1
-            step = index.step if not None else 1
-            if start in range(self.__logical_size) and start+step in range(self.__logical_size):
+            start: int = index.start if not None else 0
+            stop: int = index.stop if not None else -1
+            step:int = index.step if not None else 1
+            if start not in range(self.__logical_size) or stop not in range(self.__logical_size):
                 raise IndexError("Index must be in array")
             sliced_items = self.__items[start:stop:step]
-            return Array(starting_sequence=list(sliced_items), data_type=self.__data_type) 
+            return Array(starting_sequence=sliced_items.tolist(), data_type=self.__data_type) 
         elif isinstance(index, int):
             if index not in range(self.__logical_size):
                 raise IndexError("Index must be in array")
             item = self.__items[index]
             return item.item() if isinstance(item, np.generic) else item
+        else: 
+            raise TypeError("Index must be an int or a slice")
         
     def __setitem__(self, index: int, item: T) -> None:
         if not isinstance(item, self.__data_type):
@@ -79,18 +86,57 @@ class Array(IArray[T]):
         else:
             self.__items[index] = item
                
+    
 
     def append(self, data: T) -> None:
-        raise NotImplementedError('Append not implemented.')
+        app_index: int = self.__logical_size + 1
+        if self.__logical_size == self.__physical_size:
+            new_size = self.__physical_size * 2
+            new_array: NDArray = np.empty(new_size, dtype=self.__data_type) 
+            for i in range(len(self.__items)):
+                new_array[i] = deepcopy(self.__items[i])
+            self.__items = new_array
+            self.__physical_size = new_size
+        self.__items[app_index] = data
+        self.__logical_size += 1 
 
     def append_front(self, data: T) -> None:
-        raise NotImplementedError('Append front not implemented.')
+        if self.__logical_size == self.__physical_size:
+            new_size = self.__physical_size * 2
+            new_array: NDArray = np.empty(new_size, dtype=self.__data_type)
+            new_array[0] = data 
+            for i in range(len(self.__items)):
+                new_array[i+1] = deepcopy(self.__items[i])
+            self.__items = new_array
+            self.__physical_size = new_size
+        else:
+            new_array: NDArray = np.empty(self.__physical_size, dtype=self.__data_type)
+            new_array[0] = data
+            for i in range(len(self.__items)):
+                new_array[i+1] = deepcopy(self.__items[i])
+                self.__items = new_array
+        self.__logical_size += 1
 
     def pop(self) -> None:
-        raise NotImplementedError('Pop not implemented.')
+        self.__logical_size -=1
+        if self.__logical_size <= self.__physical_size//4:
+            new_size = self.__physical_size//4
+            new_array: NDArray = np.empty(new_size, dtype=self.__data_type) 
+            for i in range(self.__logical_size):
+                new_array[i] = deepcopy(self.__items[i])
+            self.__items = new_array
+            self.__physical_size = new_size
+        else:
+            new_array: NDArray = np.empty(self.__physical_size, dtype=self.__data_type) 
+            for i in range(self.__logical_size):
+                new_array[i] = deepcopy(self.__items[i])
+            self.__items = new_array
     
     def pop_front(self) -> None:
-        raise NotImplementedError('Pop front not implemented.')
+        if len(self.__items) == 0:
+            raise IndexError("Cannot remove object from empty array")
+        else:
+            self.__delitem__(index=0)
 
     def __len__(self) -> int: 
         return(self.__logical_size)
@@ -109,21 +155,44 @@ class Array(IArray[T]):
 	    return iter(self.__items)
     
     def __reversed__(self) -> Iterator[T]:
-        pass
+        reversed_list = list(self.__items[::-1])
+        reversed_array: NDArray = np.array(reversed_list, dtype=self.__data_type)
+        return(reversed_array)
+
     def __delitem__(self, index: int) -> None:
-       raise NotImplementedError('Delete not implemented.')
+        for i in range(self.__logical_size):
+            if i >= index and i+1 in range(len(self.__items)):
+               self.__items[i] = self.__items[i+1]
+        self.__logical_size -= 1
+        if self.__logical_size <= self.__physical_size//4:
+            new_size = self.__physical_size//4
+            new_array: NDArray = np.empty(new_size, dtype=self.__data_type) 
+            for i in range(self.__logical_size):
+                new_array[i] = deepcopy(self.__items[i])
+            self.__items = new_array
+            self.__physical_size = new_size
+        else:
+            new_array: NDArray = np.empty(self.__physical_size, dtype=self.__data_type) 
+            for i in range(self.__logical_size):
+                new_array[i] = deepcopy(self.__items[i])
+            self.__items = new_array
+            
+
+               
 
     def __contains__(self, item: Any) -> bool:
-        any(self.__items[i] is item for i in range(self.__logical_size))
+        return any(self.__items[i] == item for i in range(len(self.__items)))
 
     def clear(self) -> None:
-        raise NotImplementedError('Clear not implemented.')
+        self.__items = np.array([], dtype=self.__data_type)
+        self.__physical_size = 0
+        self.__logical_size = 0
 
     def __str__(self) -> str:
-        return '[' + ', '.join(str(item) for item in self) + ']'
+        return '[' + ', '.join(str(item) for item in self.__items) + ']'
     
     def __repr__(self) -> str:
-        return f'Array {self.__str__()}, Logical: {self.__item_count}, Physical: {len(self.__items)}, type: {self.__data_type}'
+        return f'Array {self.__str__()}, Logical: {self.__logical_size}, Physical: {len(self.__items)}, type: {self.__data_type}'
     
 
 if __name__ == '__main__':
