a6b2d6b6542241455cf3115d67fb54d75381b4f8
RomanWylie <143564289+RomanWylie@users.noreply.github.com>
2025-03-11 22:56:36 -0700

    Project 2

--- projects/project2/program.py
+++ projects/project2/program.py
@@ -1,8 +1,11 @@
-
+from datastructures.array2d import Array2D
+from projects.project2.controller import Game
 
 def main():
+    controller = Game()
+    controller.play_life()
     
-    print("Hello, World!")
+    
 
 
 
