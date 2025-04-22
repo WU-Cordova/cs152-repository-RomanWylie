e0f43dbeab2f813e79fab1c84071017928b4e219
RomanWylie <143564289+RomanWylie@users.noreply.github.com>
2025-04-22 10:36:33 -0700

    arraystack completed

---
+++ datastructures/arraystack.py
@@ -0,0 +1,271 @@
+import os
+
+from datastructures.array import Array, T
+from datastructures.istack import IStack
+
+class ArrayStack(IStack[T]):
+    ''' ArrayStack class that implements the IStack interface. The ArrayStack is a 
+        fixed-size stack that uses an Array to store the items.'''
+    
+    def __init__(self, max_size: int = 0, data_type=object) -> None:
+        empty_list:list=[data_type() for x in range(max_size)]
+        self.__stack = Array(starting_sequence=empty_list, data_type=data_type)
+        self.__top=-1
+        self.__size=0
+        self.__capacity=len(self.__stack)
+        self.__type=data_type
+
+    def push(self, item: T) -> None:
+        if self.__size == self.__capacity:
+            raise IndexError("Stack is full")
+        else:
+            self.__top +=1
+            self.__stack[self.__top]=item
+            self.__size+=1
+
+
+
+
+
+    def pop(self) -> T:
+        ''' Pops an item from the stack.
+
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.push(1)
+                >>> s.push(2)
+                >>> s.push(3)
+                >>> s.pop()
+                3
+                >>> s.pop()
+                2
+                >>> s.pop()
+                1
+                >>> s.empty
+                True
+                >>> print(repr(s))
+                ArrayStack(5): items: []
+                >>> s.pop()
+                IndexError('Stack is empty')
+        
+            Returns:
+                T -- The item popped from the stack.
+        '''
+        if self.__size==0:
+            raise IndexError("Stack is Empty")
+        else:
+            return_val = self.__stack[self.__top]
+            self.__stack[self.__top] = self.__type()
+            self.__top -= 1
+            self.__size -= 1
+            return return_val
+
+    def clear(self) -> None:
+       ''' Clears the stack. 
+       
+           Examples:
+               >>> s = ArrayStack(max_size=5, data_type=int)
+               >>> s.push(1)
+               >>> s.push(2)
+               >>> s.push(3)
+               >>> s.clear()
+               >>> s.empty
+               True
+               >>> print(repr(s))
+               ArrayStack(5): items: []
+        '''
+       for item in self.__stack:
+           item=self.__type()
+           self.__top=-1
+           self.__size=0
+    
+    @property
+    def peek(self) -> T:
+        ''' Returns the top item on the stack without removing it.
+        
+            Returns:
+                T -- The top item on the stack.
+
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.push(1)
+                >>> s.push(2)
+                >>> s.push(3)
+                >>> s.peek
+                3
+                >>> s.pop()
+                3
+                >>> s.peek
+                2
+                >>> s.pop()
+                2
+                >>> s.peek
+                1
+                >>> s.pop()
+                1
+                >>> s.empty
+                True
+                >>> s.peek
+                IndexError('Stack is empty')
+        '''
+        return self.__stack[self.__top]
+
+    @property
+    def maxsize(self) -> int:
+        ''' Returns the maximum size of the stack. 
+
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.maxsize
+                5
+        
+            Returns:
+                int: The maximum size of the stack.
+        '''
+        raise NotImplementedError    
+    @property
+    def full(self) -> bool:
+        ''' Returns True if the stack is full, False otherwise. 
+
+            Examples:
+
+        
+            Returns:
+                bool: True if the stack is full, False otherwise.
+        '''
+        if self.__size==self.__capacity:
+            return True
+        else: 
+            return False
+
+    @property
+    def empty(self) -> bool:
+        ''' Returns True if the stack is empty, False otherwise. 
+
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.empty
+                True
+                >>> s.push(1)
+                >>> s.empty
+                False
+                >>> s.pop()
+                1
+                >>> s.empty
+                True
+        
+            Returns:
+                bool: True if the stack is empty, False otherwise.
+        '''
+        if self.__size==0:
+            return True
+        else:
+            return False
+    
+    def __eq__(self, other: object) -> bool:
+        ''' Compares two stacks for equality.
+
+            Examples:
+                >>> s1 = ArrayStack(max_size=5, data_type=int)
+                >>> s2 = ArrayStack(max_size=5, data_type=int)
+                >>> s1 == s2
+                True
+                >>> s1.push(1)
+                >>> s1 == s2
+                False
+                >>> s2.push(1)
+                >>> s1 == s2
+                True
+                >>> s1.push(2)
+                >>> s2.push(3)
+                >>> s1 == s2
+                False
+        
+            Arguments:
+                other: object -- The other stack to compare.
+                
+            Returns:
+                bool -- True if the stacks are equal, False otherwise.
+        '''
+        list_self=[]
+        list_other=[]
+        for i in range(self.__capacity):
+            list_self.append(self.__stack[i])
+            list_other.append(other.__stack[i])
+        if list_self == list_other:
+            return True
+        else: 
+            return False
+       
+
+
+    def __len__(self) -> int:
+        return self.__top+1
+    
+    def __contains__(self, item: T) -> bool:
+        ''' Returns True if the item is in the stack, False otherwise.
+        
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.push(1)
+                >>> s.push(2)
+                >>> s.push(3)
+                >>> 1 in s
+                True
+                >>> 2 in s
+                True
+                >>> 3 in s
+                True
+                >>> 4 in s
+                False
+                >>> 5 in s
+                False
+            
+            Arguments:
+                item: T -- The item to search for.
+                
+            Returns:
+                bool -- True if the item is in the stack, False otherwise.
+        '''
+        list_to_check={self.__stack[i] for i in range(self.__size)}
+        if item in list_to_check:
+            return True
+        else:
+            return False
+
+    def __str__(self) -> str:
+        ''' Returns a string representation of the stack.
+
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.push(1)
+                >>> s.push(2)
+                >>> s.push(3)
+                >>> print(s)
+                [1, 2, 3]
+        
+            Returns:
+                str -- A string representation of the stack.
+        '''
+        return str([self.__stack[i] for i in range(self.__size)])
+    
+    def __repr__(self) -> str:
+        ''' Returns a string representation of the stack.
+
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.push(1)
+                >>> s.push(2)
+                >>> s.push(3)
+                >>> repr(s)
+                'ArrayStack(5): items: [1, 2, 3]'
+        
+            Returns:
+                str -- A string representation of the stack.
+        '''
+        return f"ArrayStack({self.maxsize}): items: {str(self)}"
+    
+if __name__ == '__main__':
+    filename = os.path.basename(__file__)
+    print(f'OOPS!\nThis is the {filename} file.\nDid you mean to run your tests or program.py file?\nFor tests, run them from the Test Explorer on the left.')
+
